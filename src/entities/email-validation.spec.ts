5f1484c1376425b60578d35cadb7f7ed3a287776
Gabriel Gustavo <rodriguesgg36@gmail.com>
2020-12-15 22:15:40 -0300

    test: validate emails with more than 320 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -18,6 +18,11 @@ describe('Email validation', () => {
         expect(Email.validate(email)).toBeTruthy()
     })
 
+    test('should not accept strings larger than 320 chars', () => {
+        const email = 'l'.repeat(64) + '@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
+        expect(Email.validate(email)).toBeFalsy()
+    })
+
     test('should not accept local part larger than 64 chars', () => {
         const email = 'l'.repeat(65) + '@email.com'
         expect(Email.validate(email)).toBeFalsy()
