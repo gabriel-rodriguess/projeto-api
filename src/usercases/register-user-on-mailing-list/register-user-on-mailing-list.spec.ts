d2bc36d9f2214a4cf7ca51bc3389891cf2361fab
Gabriel Gustavo <rodriguesgg36@gmail.com>
2021-01-14 23:40:27 -0300

    test: register valid user to mailing list

--- src/usercases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/usercases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,17 +1,21 @@
-import { testEnvironment } from "../../../jest.config"
-
+import { UserRepository } from './ports/user-repository'
 import { UserData } from '../../entities/user-data'
+import { InMemoryUserRepository } from './repository/in-memory-user-repository'
+import { RegisterUserOnMailingList } from './register-user-on-mailing-list'
 
 describe('Register user on mailing list use case', () => {
     test('should add user with complete data to mailing list',async () =>{
         const users: UserData[] = []
         console.log(users)
-        // const repo: UserRepository = new InMemoryUserRepository(users)
-        // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
-        // const name = 'any_name'
-        // const email = 'any@email.com'
-        // const response = await usecase.RegisterUserOnMailingList({name , email})
-        // const user = repo.findUserByEmail('any@email.com')
-        // expect((await user).name).toBe('any_name')
+         const repo: UserRepository = new InMemoryUserRepository(users)
+         const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+         const name = 'any_name'
+         const email = 'any@email.com'
+         const response = await usecase.RegisterUserOnMailingList({name , email})
+         const user = repo.findUserByEmail('any@email.com')
+         expect((await user).name).toBe('any_name')
+         expect(response.value.name).toBe('any_name')
     })
+
+    
 })
