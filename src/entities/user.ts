d40a50aec597381d216f0b61ccc539d6f2018a36
Gabriel Gustavo <rodriguesgg36@gmail.com>
2021-01-14 21:55:31 -0300

    feat: create valid user

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -6,6 +6,14 @@ import { InvalidNameError } from './errors/invalid-name-error'
 import { Name } from './name'
 
 export class User{
+    public readonly email: Email
+    public readonly name: Name
+
+    private constructor(name: Name, email: Email){
+        this.name = name
+        this.email = email
+    }
+
     static create(userData: UserData): Either<InvalidNameError | InvalidEmailError, User> {
         const nameOrError = Name.create(userData.name)
         if(nameOrError.isLeft()){
@@ -17,6 +25,10 @@ export class User{
             return left(new InvalidEmailError())
         }
 
+        const name: Name = nameOrError.value as Name
+        const email: Email = emailOrError.value as Email
+
+        return right(new User(name, email))
         
     }
 }
