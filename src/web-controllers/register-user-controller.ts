3eadf801541aae6f85389f4d9b69663bb54cceac
Gabriel Gustavo <rodriguesgg36@gmail.com>
2021-02-03 18:36:25 -0300

    feat: web controller should return 400 when user as invalid name or email

--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -1,7 +1,7 @@
 import { RegisterUserOnMailingList } from '@/usercases/register-user-on-mailing-list'
 import { HttpRequest, HttpResponse } from './ports'
 import { UserData } from '@/entities'
-import { created } from '@/web-controllers/util'
+import { created, badRequest } from '@/web-controllers/util'
 
 export class RegisterUserController {
     private readonly usecase: RegisterUserOnMailingList
@@ -13,6 +13,10 @@ export class RegisterUserController {
     public async handle(request: HttpRequest): Promise<HttpResponse> {
         const userData: UserData = request.body
         const response = await this.usecase.RegisterUserOnMailingList(userData)
+
+        if(response.isLeft()){
+            return badRequest(response.value)
+        }
 
         if(response.isRight()){
             return created(response.value)
