e5593c82e80e3f71b26f4688da9de9dcc5b71de8
Gabriel Gustavo <rodriguesgg36@gmail.com>
2021-02-25 21:54:54 -0300

    feat: add user and verify

---
+++ test/external/repositories/mongodb/mongodb-user-repository.spec.ts
@@ -0,0 +1,27 @@
+import { MongoHelper } from '@/external/repositories/mongodb/helper'
+import { MongodbUserRepository } from '@/external/repositories/mongodb'
+
+describe('Mongodb User repository', () => {
+    beforeAll(async () =>{
+        await MongoHelper.connect(process.env.MONGO_URL)
+    })
+
+    afterAll( async () => {
+        await MongoHelper.disconnect()
+    })
+
+    beforeEach( async () => {
+        MongoHelper.clearCollection('users')
+    })
+
+    test('when user is added, it should exist', async () => {
+        const userRepository = new MongodbUserRepository()
+        const user = {
+            name: 'Any name',
+            email: 'any@mail.com'
+        }
+        await userRepository.add(user)
+
+        expect(await userRepository.exists(user)).toBeTruthy()
+    })
+})
